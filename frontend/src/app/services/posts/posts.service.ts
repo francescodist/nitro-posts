750faec4e8ab75ee0e90dda89f5b0c43996ac2dd
francescodist <francesco.dstf@gmail.com>
2019-06-15 00:00:10 +0100

    post service can now return posts grouped by week (and year)

--- frontend/src/app/services/posts/posts.service.ts
+++ frontend/src/app/services/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {CrudService} from '../crud/crud.service';
 import {Post} from '../../models/post.model';
+import {PostTreeItem} from '../../components/post-tree-list/post-tree-list.component';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,7 @@ import {Post} from '../../models/post.model';
 export class PostsService {
 
   private postList: Post[] = [];
+  private groupedPostList: PostTreeItem[];
   private url = 'posts';
 
   constructor(private crudService: CrudService<Post>) {
@@ -15,11 +17,35 @@ export class PostsService {
 
   public fetchList() {
     this.crudService.getList(this.url).subscribe(list => {
-      this.postList = list;
+      this.postList = list.map(post => new Post(post));
+      this.setGroupedList(this.postList);
     });
   }
 
   public getList(): Post[] {
     return this.postList;
+  }
+
+  public getGroupedList(): PostTreeItem[] {
+    return this.groupedPostList;
+  }
+
+  private setGroupedList(postList: Post[]) {
+    this.groupedPostList = postList.reduce((postTreeList, post) => {
+      const group: PostTreeItem = postTreeList.find((postTreeItem: PostTreeItem) => {
+        return postTreeItem.groupKey === post.yearWeek;
+      });
+      if (group) {
+        group.postList.push(post);
+      } else {
+        const newGroup: PostTreeItem = {
+          groupKey: post.yearWeek,
+          groupTitle: `Week ${post.week} (${post.year})`,
+          postList: [post]
+        };
+        postTreeList.push(newGroup);
+      }
+      return postTreeList;
+    }, []);
   }
 }
