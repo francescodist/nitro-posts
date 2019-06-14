188804ced2c1b762c26fce3082390e725e64b1a9
francescodist <francesco.dstf@gmail.com>
2019-06-14 21:01:36 +0100

    added page for posts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'posts',
+    loadChildren: () => import('./pages/post-page/post-page.module').then(m => m.PostPageModule)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'posts'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
