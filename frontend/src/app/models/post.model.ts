3b0d6d2c99f0fc1f71a3773bc07a60285f869f47
francescodist <francesco.dstf@gmail.com>
2019-06-14 23:58:46 +0100

    added methods to get year and week of a post

--- frontend/src/app/models/post.model.ts
+++ frontend/src/app/models/post.model.ts
@@ -1,7 +1,32 @@
+import * as moment from 'moment';
+
 export class Post {
   id: number;
   location: string;
   time: string;
   author: string;
   text: string;
+
+  constructor(post: Post) {
+    this.id = post.id;
+    this.location = post.location;
+    this.time = post.time;
+    this.author = post.author;
+    this.text = post.text;
+  }
+
+
+  get yearWeek() {
+    return this.week + '_' + this.year;
+  }
+
+  get year() {
+    const time: moment.Moment = moment(parseInt(this.time) * 1000);
+    return time.year();
+  }
+
+  get week() {
+    const time: moment.Moment = moment(parseInt(this.time) * 1000);
+    return time.week();
+  }
 }
