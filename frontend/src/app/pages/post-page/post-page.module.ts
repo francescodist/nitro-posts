6453e1e3ea84e5a697ba956c6bfecb4a957f6c1c
francescodist <francesco.dstf@gmail.com>
2019-06-15 11:07:37 +0100

    styled post page

--- frontend/src/app/pages/post-page/post-page.module.ts
+++ frontend/src/app/pages/post-page/post-page.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PostPageRoutingModule } from './post-page-routing.module';
 import { PostPageComponent } from './post-page.component';
 import {PostTreeListModule} from '../../components/post-tree-list/post-tree-list.module';
+import {MatToolbarModule} from "@angular/material";
 
 @NgModule({
   declarations: [PostPageComponent],
   imports: [
     CommonModule,
     PostPageRoutingModule,
-    PostTreeListModule
+    PostTreeListModule,
+    MatToolbarModule
   ]
 })
 export class PostPageModule { }
