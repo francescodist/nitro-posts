d5e6d2d645ce2a60d4ccb51818fa8aca3adc3cc5
francescodist <francesco.dstf@gmail.com>
2019-06-15 15:55:55 +0100

    added unit tests for post tree list

--- frontend/src/app/components/post-tree-list/post-tree-list.component.spec.ts
+++ frontend/src/app/components/post-tree-list/post-tree-list.component.spec.ts
@@ -1,6 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
-import { PostTreeListComponent } from './post-tree-list.component';
+import {PostTreeItem, PostTreeListComponent} from './post-tree-list.component';
+import {PostGroupRadioComponent} from './post-group-radio/post-group-radio.component';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatInputModule, MatRadioModule
+} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import {By} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Post} from '../../models/post.model';
 
 describe('PostTreeListComponent', () => {
   let component: PostTreeListComponent;
@@ -8,9 +21,20 @@ describe('PostTreeListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PostTreeListComponent ]
+      declarations: [PostTreeListComponent, PostGroupRadioComponent],
+      imports: [
+        MatExpansionModule,
+        MatIconModule,
+        FormsModule,
+        MatButtonModule,
+        MatInputModule,
+        MatDividerModule,
+        MatCardModule,
+        MatRadioModule,
+        BrowserAnimationsModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +46,43 @@ describe('PostTreeListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render an expansion panel for every post group', () => {
+    component.treeList = [
+      {groupKey: 'key1'} as PostTreeItem,
+      {groupKey: 'key2'} as PostTreeItem
+    ];
+    fixture.detectChanges();
+    const expansionPanels = fixture.debugElement.queryAll(By.css('mat-expansion-panel'));
+    expect(expansionPanels.length).toBe(2);
+  });
+
+  it('should show a list of posts in the expansion panel', () => {
+    component.treeList = [
+      {groupKey: 'key1', postList: [{}, {}, {}]} as PostTreeItem,
+    ];
+    fixture.detectChanges();
+    const expansionPanel = fixture.debugElement.query(By.css('mat-expansion-panel'));
+    const postItems = expansionPanel.queryAll(By.css('.post-item'));
+    expect(postItems.length).toBe(3);
+  });
+
+  it('should show input fields when a post is being edited', fakeAsync(async () => {
+    const postToEdit = {author: 'author'} as Post;
+    component.treeList = [
+      {groupKey: 'key1', postList: [postToEdit, {}, {}]} as PostTreeItem,
+    ];
+    fixture.detectChanges();
+    let inputFields = fixture.debugElement.queryAll(By.css('mat-form-field input'));
+    expect(inputFields.length).toBe(0);
+    component.editablePost = postToEdit;
+    fixture.detectChanges();
+    tick();
+    inputFields = fixture.debugElement.queryAll(By.css('mat-form-field input'));
+    expect(inputFields.length).toBe(2);
+    const authorInputField = inputFields.find(input => {
+      return (input.nativeElement as HTMLInputElement).value === 'author';
+    });
+    expect(authorInputField).toBeTruthy();
+  }));
 });
