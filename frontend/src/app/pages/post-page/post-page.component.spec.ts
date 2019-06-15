fef79e5c08f30328889afde4fce207cc219a1337
francescodist <francesco.dstf@gmail.com>
2019-06-15 16:04:53 +0100

    fixed auto generated tests

--- frontend/src/app/pages/post-page/post-page.component.spec.ts
+++ frontend/src/app/pages/post-page/post-page.component.spec.ts
@@ -1,19 +1,31 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PostPageComponent } from './post-page.component';
+import {PostPageComponent} from './post-page.component';
+import {PostTreeListModule} from '../../components/post-tree-list/post-tree-list.module';
+import {MatToolbarModule} from "@angular/material";
+import {PostsService} from "../../services/posts/posts.service";
+import createSpy = jasmine.createSpy;
 
 describe('PostPageComponent', () => {
   let component: PostPageComponent;
   let fixture: ComponentFixture<PostPageComponent>;
+  let mockPostService: PostsService;
 
   beforeEach(async(() => {
+    mockPostService = new PostsService(null);
     TestBed.configureTestingModule({
-      declarations: [ PostPageComponent ]
+      declarations: [PostPageComponent],
+      imports: [
+        PostTreeListModule,
+        MatToolbarModule,
+      ],
+      providers: [{provide: PostsService, useValue: mockPostService}]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
+    mockPostService.fetchList = createSpy('fetchList');
     fixture = TestBed.createComponent(PostPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +33,9 @@ describe('PostPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch post list on init', () => {
+    expect(mockPostService.fetchList).toHaveBeenCalled();
   });
 });
